199f7ee1f204d9885f75bc8ae653b8b603db3949
Lê Công <33263454+lecong290398@users.noreply.github.com>
2020-11-03 14:30:56 +0700

    Seconup

---
+++ TemplateXamarinV1/TemplateXamarinV1.Android/obj/Debug/100/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TemplateXamarinV1.Droid.MainApplication, TemplateXamarinV1.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc644cdc2e38f4365d07.MainApplication.class, crc644cdc2e38f4365d07.MainApplication.__md_methods);
+		
+	}
+}
